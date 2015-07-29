b78178e67708dedf703fdd4168efa4a3bbdc05f0
zkejie <kejie5495@163.com>
2015-07-29 08:50:40 +0800

    1

---
+++ StudentDemo/src/cn/kejie/manager/StudentManager.java
@@ -0,0 +1,17 @@
+package cn.kejie.manager;
+
+/**
+ * 
+ * @author root
+ *
+ */
+public class StudentManager {
+
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("this is the first javademo!");
+	}
+}
--- readme.txt
+++
@@ -1,6 +0,0 @@
-git is a version controlle system.
-git is free software.
-test update file.
-test update file 2.
-333
-444
